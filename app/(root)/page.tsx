d54ebb3e31a000b335ee6fafd29f5722a7104a6b
Milan Kumar Nayak <milankumarnaya113@gmail.comm>
2025-04-05 11:52:01 +0530

    setup app,file and folder structure ,routing and auth page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Home Page.
+      <div>
+      <Button>Button</Button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default page
