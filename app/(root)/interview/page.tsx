ce88bbff28bcf1e4ee3b6e5ce53d44599bc448f7
Milan Kumar Nayak <milankumarnaya113@gmail.comm>
2025-04-07 10:22:31 +0530

    implement interview generated page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,18 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <h3>Interview generation</h3>
+        <Agent 
+            // userName={user?.name!}
+            // userId={user?.id}
+            // profileImage={user?.profileURL}
+            // type="generate"
+        />
+    </>
+  )
+}
+
+export default page
